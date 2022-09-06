8951af83912b89a1581da931dee129e6a7b3a999
mildata <milda.tamasauskaite@gmail.com>
2022-09-06 10:36:57 +0200

    added modal

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import styles from './HeaderCartButton.module.css';
-import CartIcon from '../Cart/CartIcon';
+import React from "react";
+import styles from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
 
-const HeaderCartButton = () => {
-    return (
-      
-        <button className={styles.button}>
-            <span className={styles.icon}> 
-                <CartIcon />
-            </span>
-            <span> Your cart </span>
-            <span className={styles.badge}> 3 </span>
-        </button>
-        
-    );
-}
+const HeaderCartButton = (props) => {
+  return (
+    <button className={styles.button} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span> Your cart </span>
+      <span className={styles.badge}> 3 </span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
